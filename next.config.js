e9708b8835f476f7a32deb0f89729ca954037998
Taurien <michel.a.c474@gmail.com>
2022-07-01 12:55:31 -0500

    added apikey tonextconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    OPEN_EXCHANGES_KEY: process.env.OPEN_EXCHANGES_KEY
+  }
 }
 
 module.exports = nextConfig
